1505a449a144bc7275863cde747dd140574bb08c
Aimee Barciauskas <barciaai@gmail.com>
2012-05-13 12:56:45 -0400

    revised domain for action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ BestBeerHere::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'bestbeerhere.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
@@ -72,7 +72,7 @@ BestBeerHere::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "bestbeerhere.herokuapp.com",
+    domain: "herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
