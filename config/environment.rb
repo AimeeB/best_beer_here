be1cd1d9652050b45df4c4af96bd57c6ebf8b11a
Aimee Barciauskas <barciaai@gmail.com>
2012-05-13 13:29:33 -0400

    removed configuration from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BestBeerHere::Application.initialize!
-config.action_mailer.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address  => "smtp.gmail.com",
-  :port  => 25,
-  :user_name  => "barciaai@gmail.com",
-  :password  => "mypass",
-  :authentication  => :login
-}
-config.action_mailer.raise_delivery_errors = true
