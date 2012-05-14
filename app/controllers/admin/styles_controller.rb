8a0be04126cc81ddbf59dfa291175a0aa5a2d382
Aimee Barciauskas <barciaai@gmail.com>
2012-05-14 19:49:10 -0400

    basic styles and fonts

--- app/controllers/admin/styles_controller.rb
+++ app/controllers/admin/styles_controller.rb
@@ -6,17 +6,9 @@ class Admin::StylesController < AdminController
   def new
     @style = Style.new
   end
-  
-  def show
-    @style = Style.find(params[:id])
-  end
-  
-  def edit
-    @style = Style.find(params[:id])
-  end
-  
+    
   def create
-    @style = Style.new(params[:beer])
+    @style = Style.new(params[:style])
     if @style.save      
       flash[:notice] = 'Style was successfully created!'  
       return redirect_to admin_styles_path
@@ -24,6 +16,23 @@ class Admin::StylesController < AdminController
     render :action => 'new'
   end
   
+  def edit
+    @style = Style.find(params[:id])
+  end
+  
+  def update
+    @style = Style.find(params[:id])
+    if @style.save      
+      flash[:notice] = 'Style was successfully updated!'  
+      return redirect_to admin_styles_path
+    end
+    render :action => 'new'
+  end
+  
+  def show
+    @style = Style.find(params[:id])
+  end
+
   def destroy
     @style = Style.find(params[:id])
     flash[:notice] = 'Style was successfully destroyed.' if @style.destroy
